20fc6ac7b55d6fadc3ca3e75666e69062be644d0
hyvnsevng <yh189487@gmail.com>
2024-10-06 04:57:31 +0900

    feat: Spring-Intro day02

--- week01/hy/Spring-Intro/day02/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ week01/hy/Spring-Intro/day02/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,16 +1,46 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
     private final MemberService memberService;
     // 스프링 컨테이너에 한번 등록 후 계속 그거 쓰면 된다
 
+    // 스프링 빈에 MemberController 등록
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")     // url: localhost:8080/members/new(@GetMapping: 요청이 GET일 때!)
+    public String createForm() {
+        return "members/createMemberForm";  // templates/members/createMemberForm.html 렌더링
+    }
+
+    @PostMapping("/members/new")    // url: localhost:8080/members/new(@PostMapping: 요청이 POST일 때!)
+    public String create(MemberForm form) {     // html의 input으로 받은 내용이 MemberForm의 name으로 입력됨
+        Member member = new Member();   // 멤버 객체 생성
+        member.setName(form.getName()); // form에서 받은 이름을 member의 name 필드에 설정
+
+        memberService.join(member);     // 회원가입처리: memberService로 전달(회원가입 로직, 회원 리포지토리에 저장)
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();     // 회원 조회한 결과 -> members라는 List에 저장
+        model.addAttribute("members", members);     // members 리스트를 model 객체에 담아서 members라는 이름으로 view에 전달
+        return "members/memberList";                            // members/memberList.html 렌더링
     }
 }
--- week01/hy/Spring-Intro/day02/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ week01/hy/Spring-Intro/day02/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import hello.hello_spring.domain.Member;
 
+import javax.swing.text.html.Option;
+
 public interface MemberRepository {
 	Member save(Member member);	// 회원 저장
 	Optional<Member> findById(Long id);			// id로 회원 찾기
-	Optional<Member> findByName(String name);	// 이름으로 찾기 
+	Optional<Member> findByName(String name);	// 이름으로 찾기
 	List<Member> findAll();						// 저장된 모든 회원 리스트 반환
 }
