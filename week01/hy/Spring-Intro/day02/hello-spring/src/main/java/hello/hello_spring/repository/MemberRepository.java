a4fc7db4b1f0fe85b2b7e95d909fb970578bcd81
hyvnsevng <yh189487@gmail.com>
2024-10-06 00:00:52 +0900

    feat: ~4강 폴더 정리 및 소스코드 추가

---
+++ week01/hy/Spring-Intro/day02/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hello_spring.domain.Member;
+
+public interface MemberRepository {
+	Member save(Member member);	// 회원 저장
+	Optional<Member> findById(Long id);			// id로 회원 찾기
+	Optional<Member> findByName(String name);	// 이름으로 찾기 
+	List<Member> findAll();						// 저장된 모든 회원 리스트 반환
+}
