02e68435ffe6fff8e04658fa5f252fa852126bbc
mins <m2insapp@gmail.com>
2024-10-06 10:58:48 +0900

    디렉토리 구조 변경

--- week01/hdm/Spring 기본/Codes/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ week01/hdm/Spring 기본/Codes/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,2 +1,30 @@
-package hello.core.order;public class OrderServiceImpl {
+package hello.core.order;
+
+import hello.core.member.MemberRepository;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository ;
+    private final DiscountPolicy discountPolicy; // DIP / OCP 지켜주기위함.
+    // DIP, OCP를 지키는순간 객체가 없어서 NULLPOINTEXCEPTION이 뜸.
+
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
