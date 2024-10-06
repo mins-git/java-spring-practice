2754b53bd61c6376663f204576351ebc2d9d6539
ebeleey <ebeleey@gmail.com>
2024-10-06 21:07:01 +0900

    폴더 수정

--- week01/ee/spring_introduction/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ week01/ee/spring_introduction/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //시스템이 정해주는 id
+
     private String name; //고객이 회원가입할 때 적는 이름
 
     public Long getId() {
