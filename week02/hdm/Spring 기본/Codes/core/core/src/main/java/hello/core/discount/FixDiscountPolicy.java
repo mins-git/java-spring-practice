9221873ef7e800c7c4bb9129b6cbd3feffcda61b
mins <m2insapp@gmail.com>
2024-10-08 19:15:56 +0900

    feat: week02 -> 스프링: 컴포넌트 스캔

--- week02/hdm/Spring 기본/Codes/core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ week02/hdm/Spring 기본/Codes/core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,20 +2,17 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.stereotype.Component;
 
-@Component
-public class RateDiscountPolicy implements DiscountPolicy{
+public class FixDiscountPolicy implements DiscountPolicy {
 
-    private int discountPercent = 10;
-
-
+    private int discountFixAmount = 1000; // 1000원 할인
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP){
-            return price * discountPercent / 100;
+            return discountFixAmount;
         } else {
             return 0;
+
         }
     }
 }
